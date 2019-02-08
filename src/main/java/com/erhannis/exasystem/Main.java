8dac2d69a97693561c6ae5816216fcdbed642a60
erhannis <Eyeillus@gmail.com>
2019-02-08 03:12:43 -0500

    Forgot SYNC, oops.  Also added a test of the class structure.

--- src/main/java/com/erhannis/exasystem/Main.java
+++ src/main/java/com/erhannis/exasystem/Main.java
@@ -5,6 +5,17 @@
  */
 package com.erhannis.exasystem;
 
+import com.erhannis.exasystem.architecture.Exa;
+import com.erhannis.exasystem.architecture.registers.BuiltInRegisters;
+import com.erhannis.exasystem.code.Addi;
+import com.erhannis.exasystem.code.Copy;
+import com.erhannis.exasystem.code.Instruction;
+import com.erhannis.exasystem.code.Jump;
+import com.erhannis.exasystem.code.Mark;
+import com.erhannis.exasystem.code.argument.ExaNumber;
+import com.erhannis.exasystem.code.argument.Label;
+import java.util.ArrayList;
+
 /**
  *
  * @author erhannis
@@ -12,5 +23,14 @@ package com.erhannis.exasystem;
 public class Main {
   public static void main(String[] args) {
     
+  }
+  
+  public static void test() {
+    Exa xa = new Exa();
+    ArrayList<Instruction> code = new ArrayList<>();
+    code.add(new Copy(new ExaNumber(0), BuiltInRegisters.X));
+    code.add(new Mark(new Label("LOOP")));
+    code.add(new Addi(BuiltInRegisters.X, new ExaNumber(1), BuiltInRegisters.X));
+    code.add(new Jump(new Label("LOOP")));
   }
 }
---
+++ src/main/java/com/erhannis/exasystem/code/Sync.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.erhannis.exasystem.code;
+
+import lombok.Data;
+
+/**
+ * Specific to the TEC RedShift
+ *
+ * @author erhannis
+ */
+@Data
+public class Sync implements Instruction {
+}
